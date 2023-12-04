0706fe6a2d7c177240b4813120c12582c1a60e83
Dharmil Karia <karia.dh@northeastern.edu>
2023-12-03 21:05:52 -0500

    fef

--- src/users/client.js
+++ src/users/client.js
@@ -3,7 +3,7 @@ import React from "react";
 const request = axios.create({
   withCredentials: true,
 });
-export const USERS_API = `http://localhost:4000/api/users`;
+export const USERS_API = `https://kanbas-node-server-app-0njc.onrender.com/api/users`;
 export const signin = async (credentials) => {
   const response = await request.post( `${USERS_API}/signin`, credentials );
   return response.data;
